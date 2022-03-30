809b3ced9fe983360e479e2cf531cbed37eb22cd
gkuer <gkuer95@gmail.com>
2022-03-30 13:18:26 +0900

    FEAT: Pytest Application
    Former-commit-id: 22166f0a94e5ca6244424d03190e242de9426f19

--- backend/posesmart/settings.py
+++ backend/posesmart/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&pq!g+4=q=^g6gn*z%cj5ghf-h2c2v2da9ej&oxlt8bcjh(v#t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
